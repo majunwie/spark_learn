093467cf4ab390fd6316b4b763879635cec680be
majunwei <6002abmjwcd>
2021-07-09 17:13:24 +0800

    unpersist

--- src/main/java/core/Persist.scala
+++ src/main/java/core/Persist.scala
@@ -9,6 +9,7 @@ import org.apache.spark.{SparkConf, SparkContext}
  * 持久化
  * 也是转换算子，只有遇到行动算子的时候才会触发
  * 当一个rdd被持久化，下一次在使用同一个rdd的时候就会去memory或者disk中取，提升效率
+ * 解决热点数据频繁访问的效率问题
  *
  * python里面做持久化的时候，没有MEMORY_ONLY_SER和MEMORY_AND_DISK_SER这两个选项
  */
@@ -22,5 +23,9 @@ object Persist {
     println(value.getStorageLevel)
     println(value2.getStorageLevel)
     println(value3.getStorageLevel)
+
+    //清空缓存
+    value.unpersist()
+    println(value.getStorageLevel)
   }
 }
