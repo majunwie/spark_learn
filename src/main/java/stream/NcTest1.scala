ba0f9f81f71078518d16ed57a1dd26bedb70112b
majunwei <6002abmjwcd>
2021-07-14 14:27:31 +0800

    stream-test1

---
+++ src/main/java/stream/NcTest1.scala
@@ -0,0 +1,24 @@
+package stream
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.dstream.{DStream, ReceiverInputDStream}
+
+object NcTest1 {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("rdd-t").setMaster("local[*]")
+    val spark = new SparkContext(conf)
+    spark.setLogLevel("WARN")
+    val ssc: StreamingContext = new StreamingContext(spark, Seconds(5))
+
+    val data: ReceiverInputDStream[String] = ssc.socketTextStream("192.168.1.102", 9999)
+    val result: DStream[(String, Int)] = data.flatMap(_.split(",")).map((_, 1)).reduceByKey(_ + _)
+    result.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+
+    ssc.stop(true,true)
+  }
+}
