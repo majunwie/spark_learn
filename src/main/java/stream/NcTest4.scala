cb63241d34bd409d53d2053659e14dffe171df9c
majunwei <6002abmjwcd>
2021-07-14 17:59:49 +0800

    window

--- src/main/java/stream/NcTest4.scala
+++ src/main/java/stream/NcTest4.scala
@@ -17,10 +17,12 @@ object NcTest4 {
     val spark = new SparkContext(conf)
     val ssc: StreamingContext = new StreamingContext(spark, Seconds(5))
     ssc.checkpoint("./ckp")
+    ssc.sparkContext.setLogLevel("WARN")
 
     val data: ReceiverInputDStream[String] = ssc.socketTextStream("192.168.1.102", 9999)
     val result: DStream[(String, Int)] = data.flatMap(_.split(","))
-      .map((_, 1)).reduceByKeyAndWindow(_ + _,Seconds(20),Seconds(5))//必须是微批持续时间的倍数
+      .map((_, 1)).reduceByKeyAndWindow((a:Int,b:Int)=>a+b,Seconds(20),Seconds(5))//必须是微批持续时间的倍数,不能用_+_,底层推断不出类型
+
     result.print()
     ssc.start()
     ssc.awaitTermination()
