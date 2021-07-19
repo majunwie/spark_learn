d4ea0a284ecfc43f8cb2b33dd0a086ff4aca1de9
majunwei <6002abmjwcd>
2021-07-19 16:49:38 +0800

    spark-sql

---
+++ src/main/java/sql/SqlTest1.scala
@@ -0,0 +1,15 @@
+package sql
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object SqlTest1 {
+  def main(args: Array[String]): Unit = {
+    val spark: SparkSession = SparkSession.builder().master("local[*]").appName("sql-test").getOrCreate()
+    spark.sparkContext.setLogLevel("WARN")
+    val data1: DataFrame = spark.read.csv("./data.csv")
+    data1.show()
+    data1.select("_c0").show()
+    spark.stop()
+
+  }
+}
---
+++ src/main/java/sql/SqlTest2.scala
@@ -0,0 +1,38 @@
+package sql
+
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+
+object SqlTest2 {
+  def main(args: Array[String]): Unit = {
+    val spark: SparkSession = SparkSession.builder().master("local[*]").appName("sql-test").getOrCreate()
+    val sc: SparkContext = spark.sparkContext
+    sc.setLogLevel("WARN")
+    //rdd
+    val rdd: RDD[String] = sc.textFile("data.csv")
+    val personRdd: RDD[Person] = rdd.map(it => {
+      val arr: Array[String] = it.split(",")
+      Person(arr(0), Integer.valueOf(arr(1)))
+    })
+    import spark.implicits._
+    println("++++++RDD转为DataFrame++++++")
+    val df: DataFrame = personRdd.toDF()
+    df.show()
+    df.select("name").show()
+    println("++++++RDD转为DataSets++++++")
+    val ds: Dataset[Person] = personRdd.toDS()
+    ds.show()
+    ds.select("num").show()
+    println("++++++DataFrame转为DataSets++++++")
+    val df2ds: Dataset[Person] = df.as[Person]
+    df2ds.show()
+    println("++++++DataFrame转为DataSets++++++")
+    val ds2df: DataFrame = ds.toDF()
+    ds2df.show()
+    //关闭资源
+    spark.stop()
+  }
+
+  case class Person(name:String,num:Int)
+}
